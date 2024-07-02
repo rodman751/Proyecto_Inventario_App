1107daba9dd157fe254627a97fa9004616c0a271
Rodmansito <78875773+rodman751@users.noreply.github.com>
2024-07-02 10:47:39 -0500

    CRUD productos

---
+++ Inventario.MVC/Controllers/ProductosController.cs
@@ -0,0 +1,104 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Inventario.ConsumeAPI;
+using Inventario.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventario.MVC.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public INotyfService _notifyService { get; }
+        private string Productos;
+        
+        public ProductosController(IConfiguration configuration, INotyfService notyfService)
+        {
+            Productos = configuration.GetValue("ApiUrlBase", "").ToString() + "/Producto";
+            _notifyService = notyfService;
+        }
+        // GET: ProductosController
+        public ActionResult Index()
+        {
+            var data = CRUD<Producto>.Read(Productos);
+            return View(data);
+        }
+
+        // GET: ProductosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Producto collection)
+        {
+            try
+            {
+                var data = CRUD<Producto>.Created(Productos, collection);
+                _notifyService.Success("Producto creado");
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var data = CRUD<Producto>.Read_ById(Productos,id);
+            return View(data);
+        }
+
+        // POST: ProductosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Producto collection)
+        {
+            try
+            {
+                var data = CRUD<Producto>.Update(Productos, id, collection);
+                _notifyService.Information("Producto actualizado");
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductosController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        // POST: ProductosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+           
+            try
+            {
+                
+                var data = CRUD<Producto>.Delete(Productos, id);
+                _notifyService.Information("Producto eliminado");
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
